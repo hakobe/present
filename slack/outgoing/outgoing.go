2be2d9d89be43d578d83d87b9ea370d943a2e901
hakobe <hakobe@gmail.com>
2014-11-15 21:49:44 +0900

    use struct

--- slack/outgoing/outgoing.go
+++ slack/outgoing/outgoing.go
@@ -8,14 +8,14 @@ import (
 
 var bind string = ":" + os.Getenv("PRESENT_SLACK_OUTGOING_PORT")
 
-func Start() chan string {
-	out := make(chan string, 1000)
+func Start() chan struct{} {
+	out := make(chan struct{}, 1000)
 
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		if r.Method == "POST" {
 			userId := r.FormValue("user_id")
 			if userId != "USLACKBOT" {
-				out <- "ping"
+				out <- struct{}{}
 			}
 		}
 		fmt.Fprintf(rw, "ok")
