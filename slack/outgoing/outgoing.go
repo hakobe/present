289cfc9676d6ee6427efd08b538912672a56397b
hakobe <hakobe@gmail.com>
2014-12-17 14:16:11 +0900

    人間が喋ってるかどうかの判定がまちがってた

--- slack/outgoing/outgoing.go
+++ slack/outgoing/outgoing.go
@@ -19,21 +19,24 @@ func Handle(op chan *Op, rw http.ResponseWriter, r *http.Request) {
 		userId := r.FormValue("user_id")
 		text := strings.TrimSpace(r.FormValue("text"))
 		texts := regexp.MustCompile("\\s+").Split(text, -1)
-		if userId != "USLACKBOT" && len(texts) > 1 && texts[0] == config.Name {
-			switch texts[1] {
-			case "plz":
-				op <- &Op{"plz", nil}
-			case "fever":
-				op <- &Op{"fever", nil}
-			case "tags":
-				op <- &Op{"tags", nil}
-			case "add":
-				op <- &Op{"add", []string{strings.Join(texts[2:], " ")}}
-			case "del":
-				op <- &Op{"del", []string{strings.Join(texts[2:], " ")}}
-			case "help":
-				op <- &Op{"help", nil}
-			default:
+
+		if userId != "USLACKBOT" {
+			if len(texts) > 1 && texts[0] == config.Name {
+				switch texts[1] {
+				case "plz":
+					op <- &Op{"plz", nil}
+				case "fever":
+					op <- &Op{"fever", nil}
+				case "tags":
+					op <- &Op{"tags", nil}
+				case "add":
+					op <- &Op{"add", []string{strings.Join(texts[2:], " ")}}
+				case "del":
+					op <- &Op{"del", []string{strings.Join(texts[2:], " ")}}
+				case "help":
+					op <- &Op{"help", nil}
+				}
+			} else {
 				op <- &Op{"humanspeaking", nil}
 			}
 		}
